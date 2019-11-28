09d5fbd27e17c1644675d8e4946640036c2f8372
Josh Giesbrecht <josh@thoughtlost.org>
2019-11-27 19:45:49 -0800

    initial commit
    yep

---
+++ p5test.lisp
@@ -0,0 +1,8 @@
+(java:jnew-runtime-class
+	  "_1"
+	  :superclass "org.processing.PApplet"
+	  :methods
+	  `(("settings" :void ()
+			, (jcall
+			    "size" 500 500))))
+
---
+++ processing.asd
@@ -0,0 +1,4 @@
+(in-package :asdf)
+
+(defsystem processing
+    :components ((:mvn "org.processing/core/3.3.7")))
